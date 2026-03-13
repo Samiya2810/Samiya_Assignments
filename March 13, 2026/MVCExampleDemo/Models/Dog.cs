4f6e7919449c8ab3c2ab9d9980e515ef0b926f69
Samiya2810 <samiyamalhotra2803@gmail.com>
2026-03-13 16:09:00 +0530

    MVC1

---
+++ March 13, 2026/MVCExampleDemo/Models/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCExampleDemo.Models
+{
+    public class Dog
+    {
+        [Required(ErrorMessage="ID is required")]
+
+        public int ID { get; set; }
+        [Required(ErrorMessage ="name is required"), MaxLength(222)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "age is required"), Range(0,20,ErrorMessage ="age should be between 0 to 20 only")]
+        public int Age {  get; set; }
+
+    }
+}
