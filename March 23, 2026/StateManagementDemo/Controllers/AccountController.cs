208a3eba5e3267c6b70e0192f6f446558cc519a9
Samiya2810 <samiyamalhotra2803@gmail.com>
2026-03-24 15:38:13 +0530

    statemanagement

---
+++ March 23, 2026/StateManagementDemo/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StateManagementDemo.Models;
+namespace StateManagementDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //var cookieOptions = new CookieOptions
+                //{
+                //    Expires = DateTime.Now.AddMinutes(1) // set cookie
+                //};
+
+                //Response.Cookies.Append("UserName", model.Username, cookieOptions);
+
+                HttpContext.Session.SetString("UserName", model.Username);
+
+                return RedirectToAction("welcome");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Welcome()
+        {
+
+            var username = HttpContext.Session.GetString("Username");
+
+
+            //if (Request.Cookies.ContainsKey("UserName"))
+            //{
+            //    string username = Request.Cookies["UserName"];
+            //    ViewBag.Username = username;
+            //}
+
+            if (!String.IsNullOrEmpty(username))
+            {
+                ViewBag.UserName = username;
+            }
+           
+            else
+            {
+                return RedirectToAction("Login");
+            }
+                return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("UserName");
+            return RedirectToAction("Login");
+        }
+    }
+}
