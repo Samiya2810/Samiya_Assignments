813af06e0af2d23d8c60d7b823c50ed18e60f3c1
Samiya2810 <samiyamalhotra2803@gmail.com>
2026-03-24 15:40:52 +0530

    webAPI

---
+++ March 24, 2026/WebAPI/Models/EmpContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+
+    public class EmpContext : DbContext
+    {
+        public EmpContext(DbContextOptions dbContextOptions) :
+            base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> employees { set; get; }
+    }
+}
